5e6c38b6a7474366d8e6d0530f88818c73fe1235
islam <islamamara062@gmail.com>
2025-06-22 18:47:28 +0100

    i edited the about.svelte

--- src/routes/componentes/Welcome.svelte
+++ src/routes/componentes/Welcome.svelte
@@ -3,6 +3,6 @@
   <h1 class="text-blue-300 text-9xl">Welcome</h1>
   <h1 class="text-gray-400 flex justify-center py-5">scroll down to see more.</h1>
   <div class="mt-20 animate-bounce">
-		<span class="text-purple-500 text-3xl justify-center flex">&#8595;</span>
+		<span class="text-blue-300 text-3xl justify-center flex">&#8595;</span>
 	</div>
 </div>
--- src/routes/componentes/about.svelte
+++ src/routes/componentes/about.svelte
@@ -1,5 +1,15 @@
 <div class="text-white mt-50 mr-150 text-xl pl-12">
-  <h1>Who I am ?</h1> 
+
+  <h1 class="text-6xl p-10">Who I am ?</h1> 
   <p>Hi I am My name is Islam, And I am 18 years old and I am from algeria, I work as a software enginner and i created my own projects, I am speak arabic and english and french.</p>
+
+</div>
+
+<div class="text-white">
+  <div class="bg-neutral-900 rounded">
+    <h2>Hobbies</h2>
+    <p>Computer Enginner, Going to the Gym and nature</p>
+  </div>
+
 
 </div>
