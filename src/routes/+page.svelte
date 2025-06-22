c93793ffa263caa5c294e608073e096cdc1da306
islam <islamamara062@gmail.com>
2025-06-22 09:14:33 +0100

    i just added some info about me.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import About from './componentes/about.svelte'; 
   import Welcome from './componentes/Welcome.svelte';
 </script>
-
 <div>
 
   <Welcome/>
--- src/routes/componentes/about.svelte
+++ src/routes/componentes/about.svelte
@@ -1,4 +1,5 @@
 <div class="text-white mt-50 mr-150 text-xl pl-12">
+  <h1>Who I am ?</h1> 
   <p>Hi I am My name is Islam, And I am 18 years old and I am from algeria, I work as a software enginner and i created my own projects, I am speak arabic and english and french.</p>
 
 </div>
