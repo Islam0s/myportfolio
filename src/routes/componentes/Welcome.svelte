3209aeea62c9aaab795710680cc2b53c4876e751
islam <islamamara062@gmail.com>
2025-06-25 17:13:23 +0100

    I eddited the welcome compenent for just phone screens

--- src/routes/componentes/Welcome.svelte
+++ src/routes/componentes/Welcome.svelte
@@ -1,8 +1,7 @@
-<div class="flex-col flex items-center pt-30 w-full">
-
-  <h1 class="text-blue-300 text-9xl">Welcome</h1>
-  <h1 class="text-gray-400 flex justify-center py-5">scroll down to see more.</h1>
-  <div class="mt-20 animate-bounce">
-		<span class="text-blue-300 text-3xl justify-center flex">&#8595;</span>
+<div class="flex w-full flex-col items-center pt-30">
+	<h1 class="text-6xl text-blue-300">Welcome</h1>
+	<p class=" flex justify-center py-5 text-gray-400">scroll down to see more.</p>
+	<div class="mt-20 animate-bounce">
+		<span class="flex justify-center text-3xl text-blue-300">&#8595;</span>
 	</div>
 </div>
