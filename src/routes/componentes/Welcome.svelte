d03b09a842fc2cf108be362e5ce0b1d3b0e90f31
islam <islamamara062@gmail.com>
2025-06-25 17:27:28 +0100

    I maked the welcome component responsiv

--- src/routes/componentes/Welcome.svelte
+++ src/routes/componentes/Welcome.svelte
@@ -1,5 +1,5 @@
 <div class="flex w-full flex-col items-center pt-30">
-	<h1 class="text-6xl text-blue-300">Welcome</h1>
+	<h1 class="lg:text-10 xl:text-12xl text-6xl text-blue-300 md:text-8xl">Welcome</h1>
 	<p class=" flex justify-center py-5 text-gray-400">scroll down to see more.</p>
 	<div class="mt-20 animate-bounce">
 		<span class="flex justify-center text-3xl text-blue-300">&#8595;</span>
