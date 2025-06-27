6e1beb5814f232b1f7e1b1ce7dbf503c56545a99
islam <islamamara062@gmail.com>
2025-06-27 17:48:47 +0100

    A small changes in about.svetle

--- src/routes/componentes/about.svelte
+++ src/routes/componentes/about.svelte
@@ -1,3 +1,7 @@
+<script>
+	import '../../app.css';
+</script>
+
 <div class="mt-50 mr-150 text-xl text-white md:text-2xl">
 	<h1 class="w-70 p-10 text-4xl md:w-140 md:text-6xl">Who I am ?</h1>
 	<p class="w-70 pl-5 md:w-180">
@@ -6,20 +10,14 @@
 	</p>
 </div>
 
-<div class="flex text-white">
-	<div
-		id="firstbox"
-		class="mt-20 ml-32 h-48 w-54 rounded-xl border border-yellow-800/30 bg-neutral-900 p-6"
-	>
+<div class="text-white md:flex">
+	<div id="firstbox" class="box_phone">
 		<h2 class="mb-3">Hobbies</h2>
 		<p>Computer Enginner, Going to the Gym and nature</p>
 	</div>
 
 	<div class="text-white">
-		<div
-			id="secondbox"
-			class="mt-20 ml-20 h-48 w-54 rounded-xl border border-green-800/30 bg-neutral-900 p-6"
-		>
+		<div id="secondbox" class="box_phone">
 			<h2 class="mb-3">Jokes</h2>
 			<p>this text for just as an example to see if it is working</p>
 		</div>
@@ -27,19 +25,13 @@
 </div>
 
 <div class="flex text-white">
-	<div
-		id="therdbox"
-		class="mt-20 ml-32 h-48 w-54 rounded-xl border border-blue-800/30 bg-neutral-900 p-6"
-	>
+	<div id="therdbox" class="box_phone">
 		<h2 class="mb-3">Hobbies</h2>
 		<p>Computer Enginner, Going to the Gym and nature</p>
 	</div>
 
 	<div class="text-white">
-		<div
-			id="forthbox"
-			class="mt-20 ml-20 h-48 w-54 rounded-xl border border-yellow-800/30 bg-neutral-900 p-6"
-		>
+		<div id="forthbox" class="box_phone">
 			<h2 class="mb-3">Jokes</h2>
 			<p>this text for just as an example to see if it is working</p>
 		</div>
