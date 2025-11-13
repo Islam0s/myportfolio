449b658a5e6acc788883aaf9ce419dc91099bf81
Hexbyte <islamamara062@gmail.com>
2025-11-13 16:01:41 +0000

    trying to a fix a problem in deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
+      fallback: 'index.html'
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/myportfolio' : '',
+      base: process.env.NODE_ENV === 'production' ? '/myportfolio' : ''
     }
   }
 };
+
+export default config;
 
